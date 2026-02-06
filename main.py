90763d137db4ecc822c4572d4844f8cab251af7b
Robin Meersman <echteenrobin@gmail.com>
2026-02-06 17:16:36 +0100

    feat: initial algorithm

---
+++ main.py
@@ -0,0 +1,56 @@
+import argparse as ap
+from PIL import Image
+import imagehash
+import os
+
+
+def arg_parser() -> ap.Namespace:
+    parser = ap.ArgumentParser(description='Image deduplication tool')
+    parser.add_argument('--input_dir', '-i', type=str, help='Input directory')
+    parser.add_argument('--output_file', '-o', type=str, default='./duplicates.txt', help='Output file')
+    return parser.parse_args()
+
+def compute_hashes(input_dir: str) -> dict:
+    hashes = dict()
+
+    for f in os.listdir(input_dir):
+        image_path = os.path.join(input_dir, f)
+        if os.path.isfile(image_path):
+            hashes[f] = imagehash.average_hash(Image.open(image_path))
+
+    return hashes
+
+
+def compute_duplicates(hashes: dict[str, imagehash.ImageHash]) -> list[tuple[str, str]]:
+    keys = list(hashes.keys())
+    duplicates: list[tuple[str, str]] = list()
+    boundary = 5
+
+    for i in range(len(keys)):
+        for j in range(i + 1, len(keys)):
+            h1 = hashes[keys[i]]
+            h2 = hashes[keys[j]]
+
+            # skip if images are not of the same size --> likely not similar
+            if h1.hash.size != h2.hash.size:
+                continue
+
+            distance = h1 - h2
+
+            # similarity boundary at 90% equal
+            if distance <= boundary:
+                duplicates.append((keys[i], keys[j]))
+
+    return duplicates
+
+
+if __name__ == '__main__':
+    args = arg_parser()
+
+    image_hashes = compute_hashes(args.input_dir)
+
+    duplicates = compute_duplicates(image_hashes)
+
+    with open(args.output_file, 'w') as f:
+        for f1, f2 in duplicates:
+            f.write(f'{f1},{f2}\n')
